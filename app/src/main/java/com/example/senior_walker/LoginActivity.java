0091060d2f7582bf114c77d96963d3a1094f2e54
Bejita123 <bsj940528@gmail.com>
2019-05-24 12:50:38 +0900

    login

--- app/src/main/java/com/example/senior_walker/LoginActivity.java
+++ app/src/main/java/com/example/senior_walker/LoginActivity.java
@@ -2,19 +2,34 @@ package com.example.senior_walker;
 
 import android.content.Intent;
 import android.os.Bundle;
+import android.support.annotation.NonNull;
 import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.ButtonBarLayout;
 import android.view.View;
+import android.widget.EditText;
+import android.widget.RelativeLayout;
 import android.widget.TextView;
 
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
+import com.google.firebase.auth.AuthResult;
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseUser;
+import static com.example.senior_walker.Utill.showToast;
+
 public class LoginActivity extends AppCompatActivity {
+    private FirebaseAuth mAuth;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_login);
 
-        TextView registerButton = (TextView)findViewById(R.id.regButton);
+        mAuth = FirebaseAuth.getInstance();
+        TextView loginBtn = (TextView)findViewById(R.id.loginBtn);
+        TextView registerButton = (TextView)findViewById(R.id.toRegBtn);
         registerButton.setOnClickListener(new View.OnClickListener() {
+
             @Override
             public void onClick(View v) {
                 Intent registerIntent = new Intent(LoginActivity.this, RegisterActivity.class);
@@ -22,5 +37,55 @@ public class LoginActivity extends AppCompatActivity {
             }
         });
 
+        loginBtn.setOnClickListener(new View.OnClickListener() {
+
+            @Override
+            public void onClick(View v) {
+
+                login();
+            }
+        });
+
+    }
+
+    private void login() {
+        String email = ((EditText) findViewById(R.id.idText)).getText().toString();
+        String password = ((EditText) findViewById(R.id.passwordText)).getText().toString();
+
+        if (email.length() > 0 && password.length() > 0) {
+            final RelativeLayout loaderLayout = findViewById(R.id.loaderLyaout);
+           // loaderLayout.setVisibility(View.VISIBLE);
+            mAuth.signInWithEmailAndPassword(email, password)
+                    .addOnCompleteListener(this, new OnCompleteListener<AuthResult>() {
+                        @Override
+                        public void onComplete(@NonNull Task<AuthResult> task) {
+                           //loaderLayout.setVisibility(View.GONE);
+                            if (task.isSuccessful()) {
+                                FirebaseUser user = mAuth.getCurrentUser();
+                                showToast(LoginActivity.this, "로그인에 성공하였습니다.");
+                                myStartActivity(MainActivity.class);
+                            } else {
+                                if (task.getException() != null) {
+                                    showToast(LoginActivity.this, task.getException().toString());
+                                }
+                            }
+                        }
+                    });
+        } else {
+            showToast(LoginActivity.this, "이메일 또는 비밀번호를 입력해 주세요.");
+        }
+    }
+
+    private void myStartActivity(Class c) {
+        Intent intent = new Intent(this, c);
+        intent.addFlags(Intent.FLAG_ACTIVITY_CLEAR_TOP);
+        startActivity(intent);
+    }
+    @Override
+    public void onBackPressed() {
+        super.onBackPressed();
+        moveTaskToBack(true);
+        android.os.Process.killProcess(android.os.Process.myPid());
+        System.exit(1);
     }
 }
--- app/src/main/java/com/example/senior_walker/RegisterActivity.java
+++ app/src/main/java/com/example/senior_walker/RegisterActivity.java
@@ -1,13 +1,83 @@
 package com.example.senior_walker;
 
 import android.os.Bundle;
+import android.support.annotation.NonNull;
 import android.support.v7.app.AppCompatActivity;
+import android.view.View;
+import android.content.Intent;
+import android.widget.EditText;
+import android.widget.RelativeLayout;
+import android.widget.TextView;
+
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
+import com.google.firebase.auth.AuthResult;
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseUser;
+
+import static com.example.senior_walker.Utill.showToast;
 
 public class RegisterActivity extends AppCompatActivity {
+    private FirebaseAuth mAuth;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_register);
+        mAuth = FirebaseAuth.getInstance();
+
+        TextView registerButton = (TextView)findViewById(R.id.regBtn);
+        registerButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent registerIntent = new Intent(RegisterActivity.this, LoginActivity.class);
+                signUp();
+               // RegisterActivity.this.startActivity(registerIntent);
+            }
+        });
+
     }
+
+    private void signUp() {
+        String email = ((EditText)findViewById(R.id.idText)).getText().toString();
+        String password = ((EditText)findViewById(R.id.passwordText)).getText().toString();
+        String passwordCheck = ((EditText)findViewById(R.id.passwordCheckText)).getText().toString();
+
+        if(email.length() > 0 && password.length() > 0 && passwordCheck.length() > 0){
+            if(password.equals(passwordCheck)){
+                final RelativeLayout loaderLayout = findViewById(R.id.loaderLyaout);
+              //  loaderLayout.setVisibility(View.VISIBLE);
+                mAuth.createUserWithEmailAndPassword(email, password)
+                        .addOnCompleteListener(this, new OnCompleteListener<AuthResult>() {
+                            @Override
+                            public void onComplete(@NonNull Task<AuthResult> task) {
+                                loaderLayout.setVisibility(View.GONE);
+                                if (task.isSuccessful()) {
+                                    FirebaseUser user = mAuth.getCurrentUser();
+                                    showToast(RegisterActivity.this, "회원가입에 성공하였습니다.");
+                                    myStartActivity(LoginActivity.class);
+                                } else {
+                                    if(task.getException() != null){
+                                        showToast(RegisterActivity.this, task.getException().toString());
+                                    }
+                                }
+                            }
+                        });
+            }else{
+                showToast(RegisterActivity.this, "비밀번호가 일치하지 않습니다.");
+            }
+        }else {
+            showToast(RegisterActivity.this, "이메일 또는 비밀번호를 입력해 주세요.");
+        }
+    }
+
+    private void myStartActivity(Class c) {
+        Intent intent = new Intent(this, c);
+        intent.addFlags(Intent.FLAG_ACTIVITY_CLEAR_TOP);
+        startActivity(intent);
+    }
+
+
+
+
 }
