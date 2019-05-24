b5b78719120bb8f309eb01a978aee98008e92b16
Bejita123 <bsj940528@gmail.com>
2019-05-24 14:33:39 +0900

    Update MainActivity.java

--- app/src/main/java/com/example/senior_walker/MainActivity.java
+++ app/src/main/java/com/example/senior_walker/MainActivity.java
@@ -1,13 +1,11 @@
 package com.example.senior_walker;
 
 import android.support.v7.app.AppCompatActivity;
-
 import android.os.Bundle;
 import android.support.annotation.NonNull;
 import android.support.design.widget.BottomNavigationView;
 import android.support.v4.app.FragmentManager;
 import android.support.v4.app.FragmentTransaction;
-import android.support.v7.app.AppCompatActivity;
 import android.view.MenuItem;
 
 
